5d36bc4babcd5bc42729bd07f0ccb65acb162b86
JaeseongYoo <praster1@gmail.com>
2014-12-14 22:52:32 +0900

    Update

---
+++ BN_Data_Generator/Working_Codes_for_Cardinalities/data_by_bnlearn.R
@@ -0,0 +1,38 @@
+http://www.bnlearn.com/documentation/networks/index.html
+
+data(learning.test)
+data = learning.test
+apply(data, 2, unique)
+
+data(gaussian.test)
+data = gaussian.test
+apply(data, 2, unique)
+
+data(lizards)
+data = lizards
+apply(data, 2, unique)
+
+data(marks)
+data = marks
+apply(data, 2, unique)
+
+data(asia)
+data = asia
+apply(data, 2, unique)
+
+data(coronary)
+data = coronary
+apply(data, 2, unique)
+
+data(alarm)
+data = alarm
+apply(data, 2, unique)
+
+data(hailfinder)
+data = hailfinder
+apply(data, 2, unique)
+
+data(insurance)
+data = insurance
+apply(data, 2, unique)
+
